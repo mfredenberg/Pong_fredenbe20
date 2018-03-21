5989b4454608c4db13c9968de13843c5dfced493
mfredenberg <fredenbe20@up.edu>
2018-03-21 14:09:35 -0700

    Initial commit

---
+++ app/src/main/java/com/example/fredenbe20/pong_fredenbe20/PongGame.java
@@ -0,0 +1,8 @@
+package com.example.fredenbe20.pong_fredenbe20;
+
+/**
+ * Created by fredenbe20 on 3/21/2018.
+ */
+
+public class PongGame {
+}
---
+++ app/src/main/java/com/example/fredenbe20/pong_fredenbe20/PongMainActivity.java
@@ -0,0 +1,37 @@
+package com.example.fredenbe20.pong_fredenbe20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+/**
+ * PongMainActivity
+ *
+ * This is the activity for the Pong game. It attaches a PongAnimator to
+ * an AnimationSurface.
+ *
+ * @author Andrew Nuxoll
+ * @author Steven R. Vegdahl
+ * @version July 2013
+ *
+ */
+public class PongMainActivity extends Activity {
+
+    /**
+     * creates an AnimationSurface containing a TestAnimator.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pong_main);
+
+        // Connect the animation surface with the animator
+        AnimationSurface mySurface = (AnimationSurface) this
+                .findViewById(R.id.animationSurface);
+        mySurface.setAnimator(new TestAnimator());
+    }
+}
