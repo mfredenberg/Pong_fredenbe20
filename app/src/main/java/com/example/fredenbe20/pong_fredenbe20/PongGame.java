939e43d73adaea85925afef4f392b36a0e118124
mfredenberg <fredenbe20@up.edu>
2018-03-22 12:49:22 -0700

    Tick work

--- app/src/main/java/com/example/fredenbe20/pong_fredenbe20/PongGame.java
+++ app/src/main/java/com/example/fredenbe20/pong_fredenbe20/PongGame.java
@@ -2,6 +2,7 @@ package com.example.fredenbe20.pong_fredenbe20;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 
 /**
@@ -14,6 +15,7 @@ public class PongGame implements Animator {
     private int width = 0;
     private int height = 0;
     private int speed = 20;
+    private String difficulty = "Easy";
 
     @Override
     public int interval() {
@@ -22,7 +24,7 @@ public class PongGame implements Animator {
 
     @Override
     public int backgroundColor() {
-        return Color.rgb(0, 0,0);
+        return Color.rgb(210, 210, 210);
     }
 
     @Override
@@ -37,6 +39,18 @@ public class PongGame implements Animator {
 
     @Override
     public void tick(Canvas canvas) {
+        Paint walls = new Paint();
+        walls.setColor(Color.CYAN);
+        canvas.drawRect(0, 0, width, 30, walls );
+        canvas.drawRect(0, 0, 30, height, walls);
+        canvas.drawRect(0, height - 30, width, height, walls);
+        if(difficulty.equals("Easy")){
+            canvas.drawRect(width - 30, height/3, width, height * (2/3), walls);
+
+        } else{
+            canvas.drawRect(width - 30, (height/2) - 100, width, (height/2) + 100, walls);
+        }
+
         if(goBackwards) {
             count--;
         }
@@ -44,11 +58,14 @@ public class PongGame implements Animator {
             count++;
         }
 
-        int wNum = (count*speed)%width;
-        int hNum = (count*speed)%height;
+        int xNum = (count*speed)%width;
+        int yNum = (count*speed)%height;
 
-        if(wNum >= width){
-            goBackwards = true;
+        if(xNum >= width || xNum < 0){
+            goBackwards = !goBackwards;
+        }
+        if(yNum >= height || yNum < 0){
+            goBackwards = !goBackwards;
         }
     }
 
